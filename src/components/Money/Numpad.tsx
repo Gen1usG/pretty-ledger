f0aecbf3987b743ea9ef716198038f03c635aa89
Gen1usG <812487021@qq.com>
2020-06-09 01:51:58 +0800

    完成键盘输入逻辑

--- src/components/Money/Numpad.tsx
+++ src/components/Money/Numpad.tsx
@@ -86,78 +86,112 @@ const NumpadWrapper = styled.section`
 function Numpad() {
   const [account, setAccount] = useState('0');
   const changeAccount = (value: string) => {
-
-
+    switch (value) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if(account.length>=10){
+          return account
+        }
+        if (account === '0') {
+          return value;
+        } else {
+          return account + value;
+        }
+      case '.':
+        if (account.indexOf('.') >= 0) {
+          return account;
+        }
+        return account + '.';
+      case 'delete':
+        if (account.length === 1) {
+          return '0';
+        } else {
+          return account.slice(0, -1) || '';
+        }
+      case 'clear':
+        return '0';
+      default:
+        return '';
+    }
   };
-  return (
-    <NumpadWrapper>
-      <div className="topWrapper">
-        <div className="note">
-          <Icon name='remark'/>
-          备注:
-        </div>
-        <input id='note' type="text" placeholder='写点什么吧'/>
-        <input id='amount' type="text" disabled value={account}/>
+
+return (
+  <NumpadWrapper>
+    <div className="topWrapper">
+      <div className="note">
+        <Icon name='remark'/>
+        备注:
       </div>
+      <input id='note' type="text" placeholder='写点什么吧'/>
+      <input id='amount' type="text" disabled value={account}/>
+    </div>
 
-      <div className="numberCounter">
-        <button onClick={() => {
-          changeAccount('7');
-        }}>7
-        </button>
-        <button onClick={() => {
-          changeAccount('8');
-        }}>8
-        </button>
-        <button onClick={() => {
-          changeAccount('9');
-        }}>9
-        </button>
-        <button onClick={() => {
-          changeAccount('clear');
-        }}>清除
-        </button>
-        <button onClick={() => {
-          changeAccount('4');
-        }}>4
-        </button>
-        <button onClick={() => {
-          changeAccount('5');
-        }}>5
-        </button>
-        <button onClick={() => {
-          changeAccount('6');
-        }}>6
-        </button>
-        <button className='yellowButton'>完成</button>
-        <button onClick={() => {
-          changeAccount('1');
-        }}>1
-        </button>
-        <button onClick={() => {
-          changeAccount('2');
-        }}>2
-        </button>
-        <button onClick={() => {
-          changeAccount('3');
-        }}>3
-        </button>
+    <div className="numberCounter">
+      <button onClick={() => {
+        setAccount(changeAccount('7'));
+      }}>7
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('8'));
+      }}>8
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('9'));
+      }}>9
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('clear'));
+      }}>清除
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('4'));
+      }}>4
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('5'));
+      }}>5
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('6'));
+      }}>6
+      </button>
+      <button className='yellowButton'>完成</button>
+      <button onClick={() => {
+        setAccount(changeAccount('1'));
+      }}>1
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('2'));
+      }}>2
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('3'));
+      }}>3
+      </button>
 
-        <button onClick={() => {
-          changeAccount('.');
-        }}>.
-        </button>
-        <button onClick={() => {
-          changeAccount('0');
-        }}>0
-        </button>
-        <button onClick={() => {
-          changeAccount('delete');
-        }}><Icon name='delete'/></button>
+      <button onClick={() => {
+        setAccount(changeAccount('.'));
+      }}>.
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('0'));
+      }}>0
+      </button>
+      <button onClick={() => {
+        setAccount(changeAccount('delete'));
+      }}><Icon name='delete'/></button>
 
-      </div>
-    </NumpadWrapper>
-  );
+    </div>
+  </NumpadWrapper>
+);
 }
 
 export {Numpad};
