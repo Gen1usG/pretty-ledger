25a1e0d78f2ab47730ca30fd9278cb106fa0573a
Gen1usG <812487021@qq.com>
2020-06-17 02:03:00 +0800

    优化Nav样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,13 +8,18 @@ const NavWrapper = styled.ul`
   background: #fff;
   > li{
     width: 33.33%;
-    >a {
+    >a { 
+      border-top: 1px solid rgba(0,0,0,.2);
+      height: 57px;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
       font-size: 16px;
-      padding: 5px 0;     
+      padding: 5px 0;    
+      -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+      -webkit-user-select: none;
+      -moz-user-select: none; 
       .icon{
           width: 26px;
           height: 26px;
