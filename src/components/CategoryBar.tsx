ebbdf2c1f854c0b6ac7edbc454111227dffb085f
Gen1usG <812487021@qq.com>
2020-06-17 02:02:47 +0800

    完成Charts top-bar布局

--- src/components/CategoryBar.tsx
+++ src/components/CategoryBar.tsx
@@ -9,43 +9,50 @@ const CategoryBarWrapper = styled.ul`
   >li{
     flex-grow: 1;
     text-align: center;
+    border-left:1px solid  #343233;
     &.selected{
       color:#ffda44;
       background-color: #343233;
+    }
+    &:nth-child(1){
+      margin-left: -1px;
+      border-radius: 5px 0 0 5px;
     }
   }
 `;
 
 type Props = {
   categoryList: string[]
-  category: '-' | '+'|'week'|'month'|'year'
-  changeCategory?:(value:'-'|'+')=>void
-  changeTimeRange?:(value:'week'|'month'|'year')=>void
+  category: '-' | '+' | 'week' | 'month' | 'year'
+  changeCategory?: (value: '-' | '+') => void
+  changeTimeRange?: (value: 'week' | 'month' | 'year') => void
 } & React.HTMLAttributes<HTMLUListElement>
 
 
 const categoryHash: { [K: string]: string } = {
   '-': '支出',
   '+': '收入',
-  'week':'周',
-  'month':'月',
-  'year':'年',
+  'week': '周',
+  'month': '月',
+  'year': '年',
 };
 
 function CategoryBar(props: Props) {
   const {children, categoryList, ...reset} = props;
-  const onChangeCategory = (categoryItem:'-' | '+'|'week'|'month'|'year')=>{
-      if(props.changeCategory){
-        props.changeCategory(categoryItem as '-'|'+')
-      }else if(props.changeTimeRange){
-        props.changeTimeRange(categoryItem as 'week'|'month'|'year')
-      }
-  }
-    return (<CategoryBarWrapper {...reset}>
+  const onChangeCategory = (categoryItem: '-' | '+' | 'week' | 'month' | 'year') => {
+    if (props.changeCategory) {
+      props.changeCategory(categoryItem as '-' | '+');
+    } else if (props.changeTimeRange) {
+      props.changeTimeRange(categoryItem as 'week' | 'month' | 'year');
+    }
+  };
+  return (<CategoryBarWrapper {...reset}>
     {props.categoryList.map(categoryItem => {
       return (<li key={categoryItem}
                   className={props.category === categoryItem ? 'selected' : ''}
-                  onClick={()=>{onChangeCategory(categoryItem as '-' | '+'|'week'|'month'|'year')}}>
+                  onClick={() => {
+                    onChangeCategory(categoryItem as '-' | '+' | 'week' | 'month' | 'year');
+                  }}>
         {categoryHash[categoryItem]}</li>);
     })}
   </CategoryBarWrapper>);
--- src/views/Charts.tsx
+++ src/views/Charts.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import {Layout} from '../components/Layout';
+import styled from 'styled-components';
+import {CategoryBar} from '../components/CategoryBar';
+import {Icon} from '../components/Icon';
+const ChartsWrapper = styled.div`
+   .icon{
+      width: 14px;
+      height: 14px;
+      fill: #000;
+      overflow: hidden;
+   }
+   .top-bar{
+      background-color: #ffda44;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .category{
+        padding:5px 0;
+        font-size: 18px;
+      }
+      .categoryBar{
+        width: 90%;
+        margin:5px 0 10px 0;
+      }
+      
+   }
+`;
 
 function Charts() {
   return (
     <Layout>
-
+      <ChartsWrapper>
+        <div className="top-bar">
+          <div className="category">支出 <Icon name='down'/>  </div>
+          <CategoryBar categoryList={['week','month','year']} category='week' className={'categoryBar'}/>
+        </div>
+        <div className="main-charts"></div>
+        <div className="rank"></div>
+      </ChartsWrapper>
     </Layout>
   );
 }
